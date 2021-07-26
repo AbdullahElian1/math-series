9e7b1f9801e272077c34ea202e25bd2fe496fe1c
AbdullahElian1 <abodelian28@gmail.com>
2021-07-26 16:41:11 +0300

    add unit test for series

---
+++ math_series/series.py
@@ -0,0 +1,56 @@
+
+def fibonacci_series(n):
+    if n<0:
+       return "enter positive number"
+    elif n==0:
+        return 0
+    elif n==1:
+       return 1
+    else:
+        return(fibonacci_series(n-1)+ fibonacci_series(n-2))
+
+   
+# print(fibonacci_series(7))
+
+
+
+
+
+def lucas_numbers(n):
+#    print("x")
+   if n<0:
+       return "enter positive number"
+   elif n==0:
+        return 2
+   elif n==1:
+       return 1
+   else:
+        return(lucas_numbers(n-1)+ lucas_numbers(n-2))
+
+# print(lucas_numbers(8))
+    
+
+
+
+
+
+def sum_series(n,first=0,second=1):
+#    print(first)
+   if n<0:
+       return "enter positive number"
+   elif n==0:
+        return first
+   elif n==1:
+       return second
+   else:
+        return(sum_series(n-1,first,second)+sum_series(n-2,first,second))
+    
+
+print(sum_series(7,0,1))
+    
+#   if first==0 and second==1:
+#       return fibonacci_series(n)
+#   elif first==2 and second==1:
+#       return lucas_numbers(n)
+#   else:
+#       other(n)
--- tests/test_math_series.py
+++ tests/test_math_series.py
@@ -1,5 +1,40 @@
+from math_series.series import *
 from math_series import __version__
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+
+def test_zero():
+    expected=0
+
+    actual=fibonacci_series(0)
+
+    assert expected==actual
+
+def test_one():
+    expected=1
+
+    actual=fibonacci_series(1)
+
+    assert expected==actual
+
+def test_seven():
+    expected=13
+
+    actual=fibonacci_series(7)
+
+    assert expected==actual
+
+def test_negative_number():
+    expected= "enter positive number"
+
+    actual=fibonacci_series(-7)
+
+    assert expected==actual   
+
+
+    
+
