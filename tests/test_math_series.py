e3ba2f713a7b7ad3c534842e1287eb921bbfcd1f
AbdullahElian1 <abodelian28@gmail.com>
2021-07-26 16:41:52 +0300

    add unit test for series

--- tests/test_math_series.py
+++ tests/test_math_series.py
@@ -36,5 +36,65 @@ def test_negative_number():
     assert expected==actual   
 
 
-    
+    # test for lucas_series
+
+def test_zero_for_lucas():
+    expected=2
+
+    actual=lucas_numbers(0)
+
+    assert expected==actual
+
+def test_one_for_lucas():
+    expected=1
+
+    actual=lucas_numbers(1)
+
+    assert expected==actual
+
+def test_seven_for_lucas():
+    expected=29
+
+    actual=lucas_numbers(7)
+
+    assert expected==actual
+
+def test_negative_number_for_lucas():
+    expected= "enter positive number"
+
+    actual=lucas_numbers(-7)
+
+    assert expected==actual  
+
+
+# test for other series
+
+def test_zero_for_other():
+    expected=0
+
+    actual=sum_series(0)
+
+    assert expected==actual
+
+def test_one_for_other():
+    expected=3
+
+    actual=sum_series(1,4,3)
+
+    assert expected==actual
+
+def test_seven_for_other():
+    expected=137
+
+    actual=sum_series(7,9,5)
+
+    assert expected==actual
+
+def test_negative_number_for_other():
+    expected= "enter positive number"
+
+    actual=sum_series(-7)
+
+    assert expected==actual   
+
 
